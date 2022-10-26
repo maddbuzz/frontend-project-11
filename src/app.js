834f1634d7a2b263d155be1b5802c9a8c9c79645
Andrey Anisimov <maddbuzz@gmail.com>
2022-10-26 20:08:23 +0800

    refactor

--- src/app.js
+++ src/app.js
@@ -1,23 +1,4 @@
-import * as yup from 'yup';
-import onChange from 'on-change';
-import _keyBy from 'lodash/keyBy.js';
-import _isEmpty from 'lodash/isEmpty.js';
-import renderView from './view.js';
-
-const schema = yup.object().shape({
-  url: yup.string().required().url(),
-});
-
-const validate = (fields) => {
-  // Как только в коде появляется асинхронность, код должен менять свою структуру.
-  // В случае колбеков он становится вложенным,
-  // в случае промисов весь код превращается в непрерывную цепочку промисов.
-  // https://ru.hexlet.io/courses/js-asynchronous-programming/lessons/promises/theory_unit
-  const promise = schema.validate(fields, { abortEarly: false })
-    .then(() => ({}))
-    .catch((e) => _keyBy(e.inner, 'path'));
-  return promise;
-};
+import initView from './view.js';
 
 const app = () => {
   const elements = {
@@ -38,33 +19,7 @@ const app = () => {
     },
   };
 
-  // Контроллеры меняют модель, тем самым вызывая рендеринг.
-  const watchState = onChange(state, renderView(elements));
-
-  // Контроллеры не должны менять DOM напрямую, минуя представление.
-  elements.formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const url = formData.get('url').trim();
-    watchState.aggregator.processState = 'validatingInput';
-    // ...в случае промисов весь код превращается в непрерывную цепочку промисов:
-    validate({ url })
-      .then((validationErrors) => {
-        if (!_isEmpty(validationErrors)) {
-          watchState.aggregator.processFeedback = { failure: 'Ссылка должна быть валидным URL' };
-          return;
-        }
-        if (watchState.aggregator.feedsURLs.includes(url)) {
-          watchState.aggregator.processFeedback = { failure: 'RSS уже существует' };
-          return;
-        }
-        watchState.aggregator.processState = 'loadingFeed';
-        watchState.aggregator.feedsURLs.push(url);
-        watchState.aggregator.processFeedback = { success: 'RSS успешно загружен' };
-      })
-      .then(() => { watchState.aggregator.processState = 'waitingForInput'; })
-      .catch((err) => { throw err; });
-  });
+  initView(elements, state);
 };
 
 export default app;
--- src/view.js
+++ src/view.js
@@ -1,14 +1,38 @@
-const switchControlsDisabled = (elements, isDisabled) => {
-  const { inputEl, submitEl } = elements;
-  inputEl.disabled = isDisabled;
-  submitEl.disabled = isDisabled;
+// View (Представление) пользуется Model (Состоянием) для отрисовки
+// (добавление, изменение или удаление элементов DOM),
+// а также добавляет новые Controllers (Обработчики) в DOM.
+
+import * as yup from 'yup';
+import onChange from 'on-change';
+import _keyBy from 'lodash/keyBy.js';
+import _isEmpty from 'lodash/isEmpty.js';
+
+const schema = yup.object().shape({
+  url: yup.string().required().url(),
+});
+
+// Как только в коде появляется асинхронность, код должен менять свою структуру:
+// в случае промисов весь код превращается в непрерывную цепочку промисов.
+const validate = (fields) => {
+  const promise = schema.validate(fields, { abortEarly: false })
+    .then(() => ({}))
+    .catch((e) => _keyBy(e.inner, 'path'));
+  return promise;
+};
+
+const switchElementsDisabled = (elements, needDisable) => {
+  Object.values(elements).forEach((el) => {
+    if (needDisable) el.setAttribute('disabled', '');
+    else el.removeAttribute('disabled');
+  });
 };
 
 const handleProcessFeedback = (elements, value) => {
-  const { success, failure } = value;
   const {
     feedsEl, formEl, inputEl, feedbackEl,
   } = elements;
+  const { success, failure } = value;
+
   if (failure) {
     inputEl.classList.add('is-invalid');
     feedbackEl.classList.remove('text-success');
@@ -26,14 +50,16 @@ const handleProcessFeedback = (elements, value) => {
 };
 
 const handleProcessState = (elements, value) => {
+  const { inputEl, submitEl } = elements;
+
   switch (value) {
     case 'waitingForInput':
-      switchControlsDisabled(elements, false);
-      elements.inputEl.focus();
+      switchElementsDisabled({ inputEl, submitEl }, false);
+      inputEl.focus();
       break;
 
     case 'validatingInput':
-      switchControlsDisabled(elements, true);
+      switchElementsDisabled({ inputEl, submitEl }, true);
       break;
 
     case 'loadingFeed':
@@ -70,4 +96,34 @@ const renderView = (elements) => (path, value) => {
   }
 };
 
-export default renderView;
+const initView = (elements, state) => {
+  // Контроллеры не должны менять DOM напрямую, минуя представление.
+  // Контроллеры меняют модель, тем самым вызывая рендеринг:
+  const watchState = onChange(state, renderView(elements));
+
+  elements.formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const url = formData.get('url').trim();
+    watchState.aggregator.processState = 'validatingInput';
+    // ...в случае промисов весь код превращается в непрерывную цепочку промисов:
+    validate({ url })
+      .then((validationErrors) => {
+        if (!_isEmpty(validationErrors)) {
+          watchState.aggregator.processFeedback = { failure: 'Ссылка должна быть валидным URL' };
+          return;
+        }
+        if (watchState.aggregator.feedsURLs.includes(url)) {
+          watchState.aggregator.processFeedback = { failure: 'RSS уже существует' };
+          return;
+        }
+        watchState.aggregator.processState = 'loadingFeed';
+        watchState.aggregator.feedsURLs.push(url);
+        watchState.aggregator.processFeedback = { success: 'RSS успешно загружен' };
+      })
+      .then(() => { watchState.aggregator.processState = 'waitingForInput'; })
+      .catch((err) => { throw err; });
+  });
+};
+
+export default initView;
